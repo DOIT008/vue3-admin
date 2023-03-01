92e3fcffdbf6246710a20228827b91e0e13084c6
DOIT <zsf900730@163.com>
2023-03-01 18:52:21 +0800

    设置mock请求

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -3,7 +3,17 @@ import Mock from 'mockjs';
 Mock.setup({
   timeout: '200-600'
 });
-Mock.mock('/getData', 'get', {
-  status:200, //请求成功状态码
+Mock.mock('/api/login', 'post', () => { 
+  return {
+    status:200, //请求成功状态码
   dataList:[1,2,3,4,5,6,7,8,9,10] //模拟的请
-  })
+  }
+})
+Mock.mock('/api/userInfo','get',(options)=>{
+  console.log(options);
+  return {
+      status:200,
+      message:'请求列表成功！',
+      total:20,
+  }
+})
