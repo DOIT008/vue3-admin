d211de3ac8b53beb4f3affd0806dd91a98d19f82
DOIT <zsf900730@163.com>
2023-04-03 15:31:33 +0800

    fix: 格式化配置文件

--- src/utils/public.ts
+++ src/utils/public.ts
@@ -1,5 +1,5 @@
 import md5 from 'js-md5';
 // 加密
-export function handleMD5(account: string, pwd: string) {
+export function handleMD5(account: string, pwd: string, all:string) {
   return md5(account + pwd);
 }
